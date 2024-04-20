80a25e0659f50915070c5e7ca9f79c106aa4552c
victorcardosodev <victorcardosomainpro@gmail.com>
2024-04-20 00:11:09 -0300

    adicionando algumas tags e estizações

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    var navbar = document.querySelector('.navbar')
+    navbar.style.display = navbar.style.display === 'block' ? 'none' : 'block';
+})
